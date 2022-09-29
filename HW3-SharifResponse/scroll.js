8b8fc5e6e278dff352c7f1dc0cf477c6768ef4e4
hannahyuwen <hz2788@nyu.edu>
2022-09-28 23:46:06 -0400

    remove scroll

--- HW3-SharifResponse/scroll.js
+++ HW3-SharifResponse/scroll.js
@@ -1,6 +1,6 @@
 //https://css-tricks.com/how-to-use-the-locomotive-scroll-for-all-kinds-of-scrolling-effects/
 
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-  });
+// const scroller = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true
+//   });
