bad619e0d98b0bb273861edc5ad06492bc9f91c1
hannahyuwen <hz2788@nyu.edu>
2022-11-21 19:34:12 -0500

    add HW9

---
+++ HW9-McLuhanResponse/lights.js
@@ -0,0 +1,61 @@
+//https://www.w3schools.com/howto/howto_js_draggable.asp
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("quote"));
+dragElement(document.getElementById("quote2"));
+dragElement(document.getElementById("quote3"));
+dragElement(document.getElementById("quote4"));
+dragElement(document.getElementById("quote5"));
+
+
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+document.getElementById("light").onclick = function() {lightsOut()}
+
+function lightsOut(){
+    document.body.style.backgroundColor = "black"
+    document.getElementById("light").style.display="none"
+    document.getElementById("quote").style.display="none"
+    document.getElementById("quote2").style.display="none"
+    document.getElementById("quote3").style.display="none"
+    document.getElementById("quote4").style.display="none"
+    document.getElementById("quote5").style.display="block"
+    document.getElementById("quote6").style.display="inline"
+
+}
